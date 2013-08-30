1f9860f37a9c1bcd2ecea0ffac10169bc77a4c10
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-08-30 19:34:43 +0200

    Add kwlist-based start/1 and init/1.

--- lib/exstomp.ex
+++ lib/exstomp.ex
@@ -1,2 +1,24 @@
 defmodule ExStomp do
+  use GenServer.Behaviour
+
+  defrecordp :creds, host: "127.0.0.1", 
+                     port: 61613, 
+                     user: "admin", 
+                     pass: "password"
+
+  defrecordp :state, sock: nil
+
+  @spec start(Keyword.t) :: pid
+  def start(kwopts) do
+    opts = creds( host: kwopts[:host] || creds(creds(), :host),
+                  port: kwopts[:port] || creds(creds(), :port),
+                  user: kwopts[:user] || creds(creds(), :user),
+                  pass: kwopts[:pass] || creds(creds(), :pass) )
+    :gen_server.start_link(__MODULE__, [opts], [])
+  end
+
+  def init([opts]) do
+    IO.puts "#{__MODULE__}: init(#{inspect opts})"
+    {:ok, state()}
+  end
 end
